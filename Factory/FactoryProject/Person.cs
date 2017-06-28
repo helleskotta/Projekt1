dcd0ab2327011dd23bf526be304df18e96c4dd95
ACADEMY-7115WTQ\Administrator <karam.haroun@outlook.com>
2017-06-28 15:03:34 +0200

    Lagt till Address

--- Factory/FactoryProject/Person.cs
+++ Factory/FactoryProject/Person.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Factory
+namespace FactoryProject
 {
+
     class Person
     {
         string firstName;
         string lastName;
         string ssn;
-        //List<Address> addresses;
-        //List<PhoneNr> phoneNrs;
+        List<Address> addresses;
+
+        List<PhoneNr> phoneNrs;
     }
 }
